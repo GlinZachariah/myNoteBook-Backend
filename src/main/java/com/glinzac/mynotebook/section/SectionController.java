95f2f0e42df508b291366a5953f6c695cfcc28a0
glinzac <glinzac@gmail.com>
2020-08-29 19:23:19 +0530

    added delete and load section

--- src/main/java/com/glinzac/mynotebook/section/SectionController.java
+++ src/main/java/com/glinzac/mynotebook/section/SectionController.java
@@ -1,6 +1,7 @@
 package com.glinzac.mynotebook.section;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,37 @@ public class SectionController {
 	@Autowired
 	SectionService service;
 
+
 	/**
-	 * add a new Section
-	 * @return ResponseData
+	 * add a new section
+	 * @param model
+	 * @return
 	 */
 	@PostMapping(path = "/addNewSection")
 	public ResponseData addNewSection(@RequestBody RequestModel model) {
 		return service.saveNewSection(model.value.toString());
 	}
 //	getSection
+	
+	/** 
+	 * get all the existing section details
+	 * @return
+	 */
+	@GetMapping("/getSectionDetails")
+	public ResponseData getSectionDetails() {
+		return service.getSectionDetails();
+	}
+//	deleteSection
+	
+	/**
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/deleteSection")
+	public ResponseData deleteSection(@RequestBody RequestModel model) {
+		return service.deleteSection(model.getValue().toString());
+	}
 //	editSection
+	
+	
 }
--- src/main/java/com/glinzac/mynotebook/section/SectionService.java
+++ src/main/java/com/glinzac/mynotebook/section/SectionService.java
@@ -17,21 +17,61 @@ public class SectionService {
 	 
 
 	public ResponseData saveNewSection(String name) {
-		File file = new File( ROOT_PATH + ROOT_FILE_NAME );
-		if(!file.exists()) {
-			boolean status = file.mkdir();
-			if(status)
-				System.out.println("Root Folder Created");
-			else
-				System.out.println("ERROR CREATING ROOT FOLDER");
-		}
 		File currentFolder = new File( ROOT_PATH + ROOT_FILE_NAME + "\\"+name);
 		boolean status = currentFolder.mkdir();
 		if(status)
 			System.out.println( name + " Folder Created");
-		else
+		else {
 			System.out.println("ERROR CREATING "+ name +" FOLDER");
-		return new ResponseData(new Response("CREATED"),HttpStatus.ACCEPTED);
+			return new ResponseData(new Response("ERROR"),HttpStatus.FORBIDDEN);
+		}
+		return new ResponseData(new Response("CREATED"),HttpStatus.CREATED);
+	}
+	
+	
+	
+	public ResponseData getSectionDetails() {
+		File file = new File( ROOT_PATH + ROOT_FILE_NAME );
+		if(!file.exists()) {
+			boolean status = file.mkdir();
+			if(status) {
+				System.out.println("Root Folder Created");
+				return new ResponseData(new Response("EMPTY"),HttpStatus.OK);
+			}else
+				System.out.println("ERROR CREATING ROOT FOLDER");
+		}else {
+			String[] filesList = file.list();
+			if(filesList.length == 0) {
+				return new ResponseData(new Response("EMPTY"),HttpStatus.OK);
+			}else {
+				Response response = new Response("NOT EMPTY");
+				response.setPayLoad(filesList);
+				return new ResponseData(response,HttpStatus.OK);
+			}
+			
+		}
+		return new ResponseData(new Response("ERROR"),HttpStatus.FORBIDDEN);
+	}
+	
+	
+	public ResponseData deleteSection(String sectionName) {
+		File file = new File( ROOT_PATH + ROOT_FILE_NAME +"\\"+ sectionName);
+		System.out.println(ROOT_PATH + ROOT_FILE_NAME +"\\"+ sectionName);
+		String[] entries = file.list();
+		if(entries.length > 0) {
+			for(String s: entries){
+			    File currentFile = new File(file.getPath(),s);
+			    boolean status = currentFile.delete();
+			    if(!status)
+			    	return new ResponseData(new Response("ERROR"),HttpStatus.FORBIDDEN);
+			}
+		}
+		boolean folder = file.delete();
+		if(!folder)
+			return new ResponseData(new Response("ERROR"),HttpStatus.FORBIDDEN);
+		else
+			return new ResponseData(new Response("DELETED"),HttpStatus.OK);
+		
 	}
 
 }
